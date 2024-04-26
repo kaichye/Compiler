472212972422e6b5729ad55c67d30b75bc674dbb
kaichye <74321555+kaichye@users.noreply.github.com>
2024-04-26 15:57:23 -0400

    Add files via upload

--- build/classes/compiler/test.c
+++ build/classes/compiler/test.c
@@ -11,4 +11,29 @@ int func(int c, int d) {
 int main(void) {
 	int temp;
 	temp = func(1, 2);
+	
+	while (temp < 5) {
+		temp = temp + 1;
+	}
+	
+	if (temp == 1) {
+		if (temp == 2) {
+			temp = 11;
+		} else {
+			temp = 5;
+		}
+	} else {
+		if (temp == 3) {
+			temp = 12;
+		} else {
+			temp = 4;
+		}
+		if (temp == 100) {
+			temp = 10;
+		} else {
+			temp = 9;
+		}
+	}
+	
+	temp = 44;
 }
--- build/classes/compiler/test.s
+++ build/classes/compiler/test.s
@@ -23,6 +23,43 @@ main_bb3:
 	movl	$2, %EAX
 	movl	%EAX, %ESI
 	call	func
+	movl	$5, %EAX
+	cmpl	%EAX, %R15D
+	jge	main_bb5
 main_bb4:
+	movl	$1, %EAX
+	movl	%R15D, %EDI
+	movl	$5, %EAX
+	cmpl	%EAX, %R15D
+	jl	main_bb4
+main_bb5:
+	movl	$1, %EAX
+	cmpl	%EAX, %R15D
+	jne	main_bb8
+main_bb6:
+	movl	$2, %EAX
+	cmpl	%EAX, %R15D
+	jne	main_bb11
+main_bb9:
+main_bb7:
+main_bb18:
 	popq	%R15
 	ret
+main_bb11:
+	jmp	main_bb7
+main_bb14:
+	jmp	main_bb13
+main_bb17:
+	jmp	main_bb7
+main_bb8:
+	movl	$3, %EAX
+	cmpl	%EAX, %R15D
+	jne	main_bb14
+main_bb12:
+main_bb13:
+	movl	$100, %EAX
+	cmpl	%EAX, %R15D
+	jne	main_bb17
+main_bb15:
+main_bb16:
+	jmp	main_bb7
